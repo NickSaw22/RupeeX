b71da5f79f19d1ae8e2052446a775f0e81094f5f
NickSaw22 <swanikhil217@gmail.com>
2024-11-29 22:27:13 +0530

    moved cron expression to db

--- apps/jobs-app/src/services/TransferSchedulerService.ts
+++ apps/jobs-app/src/services/TransferSchedulerService.ts
@@ -3,28 +3,70 @@ import prisma from '@repo/db/client';
 import { transferExecutor } from './TransferExecutor';
 class TransferSchedulerService {
   public async processScheduledTransfers() {
-    const now = new Date();
-    const transfers = await prisma.scheduledTransfer.findMany({
-      where: {
-        status: 'Active',
-        nextRun: {
-          lte: now,
+    try {
+      const now = new Date();
+      const transfers = await prisma.scheduledTransfer.findMany({
+        where: {
+          status: 'Active',
+          nextRun: {
+            lte: now,
+          },
         },
-      },
-    });
+      });
 
-    await Promise.allSettled(
-      transfers.map((transfer) => 
+      await Promise.allSettled(
+        transfers.map((transfer) =>
           transferExecutor(transfer).catch((error) => {
-              console.error(`Tranfer ID ${transfer.id} failed: `, error);
+            console.error(`Tranfer ID ${transfer.id} failed: `, error);
           })
-      )
-    );
+        )
+      );
+
+      await this.updateLastRunDate();
+    }
+    catch (error) {
+      console.error(`Failed to schedule task: `, error);
+    }
+  }
+
+  public async updateLastRunDate(){
+    try{
+      const task = await prisma.scheduledTasksdt.update({
+        where:{
+          taskName: "processScheduledTransfers",
+          isActive:true
+        },
+        data:{
+          lastRun: new Date()
+        }
+      });
+    }
+    catch (error) {
+      console.error(`Failed to schedule task: `, error);
+    }
   }
 
   public async startTransferScheduler() {
     //cron.schedule('*/5 * * * *', () => this.processScheduledTransfers()); // Run every 5 minutes
-    cron.schedule('*/30 * * * * *', () => this.processScheduledTransfers()); // Run every 0.5 minute
+    //cron.schedule('*/30 * * * * *', () => this.processScheduledTransfers()); // Run every 0.5 minute
+    try{
+      const task = await prisma.scheduledTasksdt.findUnique({
+        where:{
+          taskName: "processScheduledTransfers",
+          isActive:true
+        }
+      });
+      if(task){
+        console.log("Task cron: ", JSON.stringify(task));
+        cron.schedule(task.cronExpression, ()=> this.processScheduledTransfers());
+      }
+      else{
+        console.log("No scheduler present");
+      }
+    }
+    catch (error) {
+      console.error(`Failed to schedule task: `, error);
+    }
   }
 }
 
